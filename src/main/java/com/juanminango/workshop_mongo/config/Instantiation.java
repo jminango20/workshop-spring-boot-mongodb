eeeb004abe45623b57cd0311c9dd0f50ae17883b
jminango20 <jcarlosminango@gmail.com>
2021-03-15 19:56:08 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/juanminango/workshop_mongo/config/Instantiation.java
+++ src/main/java/com/juanminango/workshop_mongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.juanminango.workshop_mongo.domain.Post;
 import com.juanminango.workshop_mongo.domain.User;
 import com.juanminango.workshop_mongo.dto.AuthorDTO;
+import com.juanminango.workshop_mongo.dto.CommentDTO;
 import com.juanminango.workshop_mongo.repository.PostRepository;
 import com.juanminango.workshop_mongo.repository.UserRepository;
 
@@ -43,6 +44,15 @@ public class Instantiation implements CommandLineRunner{
 		//354 Instanciar Post
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Sao Paulo. Abracos!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		//359
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 		//356
--- src/main/java/com/juanminango/workshop_mongo/services/PostService.java
+++ src/main/java/com/juanminango/workshop_mongo/services/PostService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.juanminango.workshop_mongo.domain.Post;
-import com.juanminango.workshop_mongo.domain.User;
 import com.juanminango.workshop_mongo.repository.PostRepository;
 import com.juanminango.workshop_mongo.services.exception.ObjectNotFoundException;
 
