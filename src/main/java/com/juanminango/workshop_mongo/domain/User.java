69cd816f26694be60b5e748057a576f4bf30d105
jminango20 <jcarlosminango@gmail.com>
2021-03-15 19:35:37 -0300

    Referenciando os Post dos Usuarios

--- src/main/java/com/juanminango/workshop_mongo/domain/User.java
+++ src/main/java/com/juanminango/workshop_mongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.juanminango.workshop_mongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user") //Mapea a DB
@@ -14,6 +17,10 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) 	//COmo estou referenciado uma coleccao nao quero carregar automaticamente os posts quando eu recuperar um usuario de la DB
+	private List<Post> post = new ArrayList<>(); //List eh uma interface
+	
 	
 	public User() {
 	}
@@ -48,6 +55,15 @@ public class User implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPost() {
+		return post;
+	}
+
+	public void setPost(List<Post> post) {
+		this.post = post;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -73,6 +89,7 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
+
 	
 	
 	
