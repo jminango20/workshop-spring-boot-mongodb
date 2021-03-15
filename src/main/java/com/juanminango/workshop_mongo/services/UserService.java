fc11e2f2564dc2913cb51341cd69a7a7ce3ad344
jminango20 <jcarlosminango@gmail.com>
2021-03-15 11:29:28 -0300

    Obtendo um usuario por id

--- src/main/java/com/juanminango/workshop_mongo/services/UserService.java
+++ src/main/java/com/juanminango/workshop_mongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.juanminango.workshop_mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.juanminango.workshop_mongo.domain.User;
 import com.juanminango.workshop_mongo.repository.UserRepository;
+import com.juanminango.workshop_mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){ //retorna todos os usuarios do meu banco
 		return repo.findAll();		
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto nao encontrado"));
 	}
 }
